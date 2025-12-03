7a09717e5548f9d64ffd1bff345ff78406f93b68
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-12-03 14:16:16 +0100

    Refactor DALIA, TeSS, and OERHub importers (#6)

--- src/oerbservatory/sources/dalia.py
+++ src/oerbservatory/sources/dalia.py
@@ -2,6 +2,7 @@
 
 import csv
 import re
+from collections.abc import Collection
 from pathlib import Path
 
 import click
@@ -12,8 +13,14 @@ from dalia_dif.dif13 import (
     OrganizationDIF13,
     parse_dif13_row,
 )
-from dalia_ingest.model import (
-    OUTPUT_DIR,
+from dalia_dif.dif13.picklists import PROFICIENCY_TO_ORDER
+from pydantic import ByteSize
+from pydantic_extra_types.language_code import _index_by_alpha3
+from rdflib import URIRef
+from tqdm import tqdm
+
+from oerbservatory.model import (
+    EN,
     Author,
     EducationalResource,
     Organization,
@@ -22,23 +29,12 @@ from dalia_ingest.model import (
     write_resources_tfidf,
     write_sqlite_fti,
 )
-from dalia_ingest.utils import DALIA_MODULE, get_dif13_paths
-from pydantic import ByteSize
-from tqdm import tqdm
+from oerbservatory.sources.utils import OUTPUT_DIR, UNSPECIFIED_OR_PROPRIETARY
 
 __all__ = [
     "get_dalia",
+    "map_dalia_oer",
 ]
-
-
-D = OUTPUT_DIR.joinpath("dalia")
-DALIA_PROCESSED_PATH = D.joinpath("dalia.jsonl")
-DALIA_TTL_PATH = DALIA_MODULE.join(name="dalia.ttl")
-DALIA_TFIDF_INDEX_PATH = D.joinpath("dalia-tfidf-index.tsv")
-DALIA_TFIDF_SIM_PATH = D.joinpath("dalia-tfidf-similarities.tsv")
-DALIA_TRANSFORMERS_INDEX_PATH = D.joinpath("dalia-transformers-index.tsv")
-DALIA_TRANSFORMERS_SIM_PATH = D.joinpath("dalia-transformers-similarities.tsv")
-DALIA_SQLITE_FTS_PATH = D.joinpath("dalia-fts-sqlite.db")
 
 ORCID_RE = re.compile(r"^\d{4}-\d{4}-\d{4}-\d{3}(\d|X)$")
 
@@ -63,26 +59,58 @@ def parse(path: str | Path) -> list[EducationalResource]:
         ]
 
 
-def _convert(e: EducationalResourceDIF13) -> EducationalResource | None:
+def _get_minimum_proficiency_level(pl: Collection[URIRef] | None) -> URIRef | None:
+    if not pl:
+        return None
+    return min(pl, key=PROFICIENCY_TO_ORDER.__getitem__)
+
+
+REMAINING_LICENSES = {
+    URIRef("https://purl.org/ontology/modalia#ProprietaryLicense"): UNSPECIFIED_OR_PROPRIETARY,
+}
+
+
+def _process_license(license_uriref: URIRef | None) -> Reference | None | URIRef:
+    if license_uriref is None:
+        return None
+    if license_uriref in REMAINING_LICENSES:
+        return REMAINING_LICENSES[license_uriref]
+    if license_uriref.startswith("http://spdx.org/licenses/"):
+        return Reference(
+            prefix="spdx", identifier=str(license_uriref).removeprefix("http://spdx.org/licenses/")
+        )
+    raise ValueError(f"unhandled license: {license_uriref}")
+
+
+def map_dalia_oer(dalia_oer: EducationalResourceDIF13) -> EducationalResource | None:
+    """Map a DALIA OER to an OERbservatory OER."""
+    languages = dalia_oer.languages
+    if languages:
+        language_alpha2 = _index_by_alpha3()[languages[0]].alpha2
+    else:
+        language_alpha2 = EN
+
+    external_uri, *external_uri_extras = dalia_oer.links
+
     rv = EducationalResource(
-        platform="dalia",
-        reference=Reference(prefix="dalia.oer", identifier=str(e.uuid)),
-        external_uri=e.links,
-        title={"en": e.title},
-        description={"en": e.description},
-        keywords=[{"en": keyword} for keyword in e.keywords],
-        authors=[_process_author(a) for a in e.authors],
-        difficulty_level=e.proficiency_levels,
-        languages=e.languages,
-        license=e.license,
-        file_formats=e.file_formats,
-        date_published=e.publication_date,
-        version=e.version,
-        audience=e.target_groups,
-        file_size=_process_size(e.file_size),
-        resource_types=e.learning_resource_types,
-        media_types=e.media_types,
-        disciplines=e.disciplines,
+        reference=Reference(prefix="dalia.oer", identifier=str(dalia_oer.uuid)),
+        external_uri=external_uri,
+        external_uri_extras=external_uri_extras or None,
+        title={language_alpha2: dalia_oer.title},
+        description={language_alpha2: dalia_oer.description} if dalia_oer.description else None,
+        keywords=[{language_alpha2: keyword} for keyword in dalia_oer.keywords],
+        authors=[_process_author(a) for a in dalia_oer.authors],
+        difficulty_level=_get_minimum_proficiency_level(dalia_oer.proficiency_levels),
+        languages=languages,
+        license=_process_license(dalia_oer.license),
+        file_formats=dalia_oer.file_formats,
+        date_published=dalia_oer.publication_date,
+        version=dalia_oer.version,
+        audience=dalia_oer.target_groups,
+        file_size=_process_size(dalia_oer.file_size),
+        resource_types=dalia_oer.learning_resource_types,
+        media_types=dalia_oer.media_types,
+        disciplines=dalia_oer.disciplines,
     )
     return rv
 
@@ -100,12 +128,12 @@ def _process_author(e: AuthorDIF13 | OrganizationDIF13) -> Author | Organization
         case AuthorDIF13():
             return Author(
                 name=e.name,
-                orcid=e.orcid,
+                orcid=e.orcid.removeprefix("https://orcid.org/") if e.orcid is not None else None,
             )
         case OrganizationDIF13():
             return Organization(
                 name=e.name,
-                ror=e.ror,
+                ror=e.ror.removeprefix("https://ror.org/") if e.ror is not None else None,
                 wikidata=e.wikidata,
             )
         case _:
@@ -114,10 +142,16 @@ def _process_author(e: AuthorDIF13 | OrganizationDIF13) -> Author | Organization
 
 def _omni_process_row(path: Path, idx: int, row: dict[str, str]) -> EducationalResource | None:
     """Convert a row in a DALIA curation file to a resource, or return none if unable."""
-    ed13 = parse_dif13_row(path, idx, row, future=True)
+    ed13 = parse_dif13_row(path.name, idx, row, future=True)
     if ed13 is None:
         return None
-    return _convert(ed13)
+    return map_dalia_oer(ed13)
+
+
+def get_dif13_paths() -> list[Path]:
+    """Get DALIA curation paths."""
+    base = Path("/Users/cthoyt/dev/dalia-curation/curation")
+    return list(base.glob("*.csv"))
 
 
 def get_dalia() -> list[EducationalResource]:
@@ -130,16 +164,23 @@ def get_dalia() -> list[EducationalResource]:
 def main(transformers: bool) -> None:
     """Process DALIA curation sheets."""
     resources = get_dalia()
-    write_resources_jsonl(resources, DALIA_PROCESSED_PATH)
+    dire = OUTPUT_DIR.joinpath("dalia")
+    dire.mkdir(exist_ok=True, parents=True)
+    write_resources_jsonl(resources, dire.joinpath("dalia.jsonl"))
 
-    write_sqlite_fti(resources, DALIA_SQLITE_FTS_PATH)
+    return
+    write_sqlite_fti(resources, dire.joinpath("dalia-fts-sqlite.db"))
 
     if transformers:
-        write_resources_tfidf(resources, DALIA_TFIDF_INDEX_PATH, DALIA_TFIDF_SIM_PATH)
+        write_resources_tfidf(
+            resources,
+            dire.joinpath("dalia-tfidf-index.tsv"),
+            dire.joinpath("dalia-tfidf-similarities.tsv"),
+        )
         write_resources_sentence_transformer(
             resources,
-            DALIA_TRANSFORMERS_INDEX_PATH,
-            DALIA_TRANSFORMERS_SIM_PATH,
+            dire.joinpath("dalia-transformers-index.tsv"),
+            dire.joinpath("dalia-transformers-similarities.tsv"),
         )
 
 
--- src/oerbservatory/sources/oersi.py
+++ src/oerbservatory/sources/oersi.py
@@ -5,14 +5,15 @@ from collections.abc import Iterable
 from typing import Any
 
 import click
-from dalia_ingest.model import (
-    EducationalResource,
-    write_resources_jsonl,
-)
-from dalia_ingest.utils import DALIA_MODULE
 from pydantic_extra_types.language_code import _index_by_alpha2
 from rdflib import URIRef
 from tqdm import tqdm
+
+from oerbservatory.model import (
+    EducationalResource,
+    write_resources_jsonl,
+)
+from oerbservatory.utils import DALIA_MODULE
 
 __all__ = [
     "get_oersi",
